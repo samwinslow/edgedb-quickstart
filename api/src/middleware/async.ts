e5a747a37052d42ca17e4683c1b7a08e5ea0177d
Sam Winslow <4645779+samwinslow@users.noreply.github.com>
2022-08-01 15:07:58 -0400

    tooling

--- api/src/middleware/async.ts
+++ api/src/middleware/async.ts
@@ -1,7 +1,10 @@
-import { Request, Response } from "express";
+import { Request, Response } from "express"
 
 export default (fn: (req: Request, res: Response, next: () => any) => any) =>
-    (req: Request, res: Response, next: () => any) => {
-      Promise.resolve(fn(req, res, next))
-        .catch(next);
-    }
+  (req: Request, res: Response, next: () => any) => {
+    Promise.resolve(fn(req, res, next))
+      .catch((err) => {
+        console.error(err)
+        next()
+      })
+  }
--- api/src/middleware/auth.ts
+++ api/src/middleware/auth.ts
@@ -6,12 +6,17 @@ import firebaseConfig from '../../config/env/staging.json'
 admin.initializeApp(firebaseConfig)
 
 export default async (req: Request, res: Response, next: () => any) => {
-  const authHeader = req.headers?.authorization
-  if (!authHeader || !authHeader.startsWith('Bearer ')) {
+  try {
+    const authHeader = req.headers?.authorization
+    if (!authHeader || !authHeader.startsWith('Bearer ')) {
+      return res.status(401).send('Unauthorized')
+    }
+    const idToken = authHeader.split('Bearer ')[1]
+    const token = await admin.auth().verifyIdToken(idToken)
+    req.user = token
+  } catch (err) {
+    console.error(err)
     return res.status(401).send('Unauthorized')
   }
-  const idToken = authHeader.split('Bearer ')[1]
-  const token = await admin.auth().verifyIdToken(idToken)
-  req.user = token
   next()
 }
