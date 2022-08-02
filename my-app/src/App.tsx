e33d53a525d44140c49c481c22b04f5032a42821
Sam Winslow <4645779+samwinslow@users.noreply.github.com>
2022-08-02 10:57:14 -0400

    minimal example, local import not working

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -1,9 +1,31 @@
-import React from 'react'
+import e from '@dbschema/local'
 
-function App() {
+const EDGEDB_SERVER_URL = 'http://localhost:3001'
+
+const runQuery = async (query: string) => {
+  const response = await fetch(EDGEDB_SERVER_URL + '/query', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({ query }),
+  })
+  return await response.json()
+}
+
+const getUsers = async (userIds: string[]) => {
+  const query = e.select(e.Person, (person) => ({
+    full_name: true
+  })).toEdgeQL()
+  const response = await runQuery(query)
+  console.log({ response })
+  return response
+}
+
+const App = () => {
   return (
     <div className="App">
-      
+      <button onClick={() => getUsers([])}>Get users</button>
     </div>
   )
 }
