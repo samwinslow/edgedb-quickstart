64a723fb2d5fcfc1e0ebf851b5ed8136b8798440
Sam Winslow <4645779+samwinslow@users.noreply.github.com>
2022-08-03 16:13:53 -0400

    timeout added to middleware

--- api/src/middleware/async.ts
+++ api/src/middleware/async.ts
@@ -1,11 +1,20 @@
 import { Request, Response } from "express"
 
+const TIMEOUT_MS = 20000
+
+const timeout = () =>
+  new Promise((_resolve, reject) => {
+    setTimeout(() => reject(new Error(`Timeout after ${TIMEOUT_MS}ms`)), TIMEOUT_MS)
+  })
+
 export default (fn: (req: Request, res: Response, next: () => any) => any) =>
   (req: Request, res: Response, next: () => any) => {
-    Promise.resolve(fn(req, res, next))
-      .catch((err) => {
-        console.error(err)
-        res.status(500).send('Internal Server Error (<traceId>)')
-        next()
-      })
+    Promise.race([
+      fn(req, res, next),
+      timeout(),
+    ]).catch((err) => {
+      console.error(err)
+      res.status(500).send('Internal Server Error (<traceId>)')
+      next()
+    })
   }
